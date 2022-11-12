b7c184fdf61363bb05d2e329eb86852248a3db6e
danijeljw-rpc <danijel@repasscloud.com>
2022-11-12 23:05:21 +1100

    add shared models

---
+++ Models/Shared/OriginalEquipmentManufacturer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using System.Runtime.Serialization;
+
+namespace api.engine_v2.Models.Shared
+{
+    public enum OriginalEquipmentManufacturer
+    {
+        [EnumMember(Value = "Dell")] Dell,
+        [EnumMember(Value = "Lenovo")] Lenovo,
+        [EnumMember(Value = "HP")] HP,
+        [EnumMember(Value = "Apple")] Apple,
+        [EnumMember(Value = "Acer")] Acer,
+        [EnumMember(Value = "Asus")] Asus,
+        [EnumMember(Value = "MSI")] MSI,
+        [EnumMember(Value = "Toshiba")] Toshiba,
+        [EnumMember(Value = "NEC")] NEC,
+        [EnumMember(Value = "IBM")] IBM,
+        [EnumMember(Value = "Compaq")] Compaq,
+        [EnumMember(Value = "Packard Bell NEC")] Packard_Bell_NEC,
+        [EnumMember(Value = "Fujitsu")] Fujitsu,
+        [EnumMember(Value = "Sharp")] Sharp,
+        [EnumMember(Value = "MSX")] MSX,
+        [EnumMember(Value = "Other")] Other
+    }
+}
+
