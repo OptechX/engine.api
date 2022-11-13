0e04824fbdefa376b0b04c8b4b22ee44f81b209f
danijeljw-rpc <danijel@repasscloud.com>
2022-11-13 15:31:07 +1100

    add enum extensions for Extend API Routes #3

---
+++ Helpers/EnumExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization;
+
+namespace Stackoverflow.Answers.Helpers;
+
+public static class EnumExtensions
+{
+    public static T GetValueFromEnumMember<T>(string value) where T : Enum
+    {
+        var type = typeof(T);
+        foreach (var field in type.GetFields())
+        {
+            var attribute = Attribute.GetCustomAttribute(field,
+                typeof(EnumMemberAttribute)) as EnumMemberAttribute;
+            if (attribute != null)
+            {
+                if (attribute.Value == value)
+                    return (T)field.GetValue(null);
+            }
+            else
+            {
+                if (field.Name == value)
+                    return (T)field.GetValue(null);
+            }
+        }
+        throw new ArgumentException($"unknow value: {value}");
+    }
+}
