6ac84fc79c02784cad5e77554b17a43e9971e79e
danijeljw-rpc <danijel@repasscloud.com>
2022-11-13 15:48:45 +1100

    update routes

--- Controllers/WindowsCapabilityController.cs
+++ Controllers/WindowsCapabilityController.cs
@@ -120,6 +120,82 @@ namespace api.engine_v2.Controllers
         {
             return (_context.WindowsCapabilities?.Any(e => e.Id == id)).GetValueOrDefault();
         }
+
+        // GET: v1/WindowsCapability/name/{name}
+        [HttpGet("name/{name}")]
+        public async Task<ActionResult<IEnumerable<WindowsCapability>>> GetWindowsCapabilityByName([FromRoute]string name)
+        {
+            var capability = _context.WindowsCapabilities.Where(a => a.Name == name);
+            
+            if (capability.Count() == 0)
+            {
+                return NotFound();
+            }
+
+            return await capability.ToListAsync();
+        }
+
+        // GET: v1/WindowsCapability/supportedwindowsversions/{version}
+        [HttpGet("supportedwindowsversions/{version}")]
+        public async Task<ActionResult<IEnumerable<WindowsCapability>>> GetWindowsCapabilityBySupportedWindowsVersions([FromRoute]string version)
+        {
+            var capability = _context.WindowsCapabilities.Where(a => a.SupportedWindowsVersions.Contains(version));
+
+            if (capability.Count() == 0)
+            {
+                return NotFound();
+            }
+
+            return await capability.ToListAsync();
+        }
+
+        // GET: v1/WindowsCapability/supportedwindowseditions/{supportedwindowsedition}
+        [HttpGet("supportedwindowseditions/{edition}")]
+        public async Task<ActionResult<IEnumerable<WindowsCapability>>> GetWindowsCapabilityBySupportedWindowsEditions([FromRoute]string edition)
+        {
+            var capability = _context.WindowsCapabilities.Where(a => a.SupportedWindowsEditions.Contains(edition));
+
+            if (capability.Count() == 0)
+            {
+                return NotFound();
+            }
+
+            return await capability.ToListAsync();
+        }
+
+        // GET: v1/WindowsCapability/supportedwindowsreleases/{supportedwindowsrelease}
+        [HttpGet("supportedwindowsreleases/{release}")]
+        public async Task<ActionResult<IEnumerable<WindowsCapability>>> GetWindowsCapabilityBySupportedWindowsReleases([FromRoute]string release)
+        {
+            var capability = _context.WindowsCapabilities.Where(a => a.SupportedWindowsReleases.Contains(release));
+
+            if (capability.Count() == 0)
+            {
+                return NotFound();
+            }
+
+            return await capability.ToListAsync();
+        }
+
+        // GET: v1/WindowsCapability/multisearch/{supportedwindowsversion}/{supportedwindowsedition}/{supportedwindowsrelease}
+        [HttpGet("multisearch/{version}/{edition}/{release}")]
+        public async Task<ActionResult<IEnumerable<WindowsCapability>>> GetWindowsCapabilityMultiSearch(
+            [FromRoute]string version,
+            [FromRoute]string edition,
+            [FromRoute]string release)
+        {
+            var capabilities = _context.WindowsCapabilities.Where(a =>
+                a.SupportedWindowsVersions.Contains(version) &&
+                a.SupportedWindowsEditions.Contains(edition) &&
+                a.SupportedWindowsReleases.Contains(release));
+            
+            if (capabilities.Count() == 0)
+            {
+                return NotFound();
+            }
+
+            return await capabilities.ToListAsync();
+        }
     }
 }
 
