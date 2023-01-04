b3481b10985bfb778632119c7ea638b291e9abba
danijeljw-rpc <danijel@repasscloud.com>
2023-01-04 13:25:13 +1100

    fix permissions

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -4,7 +4,7 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output appsettings.json appsettings.json.gpg
+--output ./appsettings.json ./appsettings.json.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output secrets/user_host_rsa secrets/user_host_rsa.gpg
+--output ./secrets/user_host_rsa ./secrets/user_host_rsa.gpg
